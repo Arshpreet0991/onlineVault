cb1610e61cd03b2242f5ced899b66e9b726e8b3b
Arshpreet0991 <arsh.singh0991@gmail.com>
2025-05-14 12:39:32 -0400

    small typo

--- src/db/databaseConnection.js
+++ src/db/databaseConnection.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       `${process.env.MONGODB_URL}/${DB_NAME}`
     );
     console.log(
-      `\n MongoDB connected!! DB Host: ${connectionInstance.connection.host}`
+      `MongoDB connected!! DB Host: ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log(error);
