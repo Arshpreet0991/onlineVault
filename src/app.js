1054d168ef7a213c2ea6d0a44550e1b70582edf8
Arshpreet0991 <arsh.singh0991@gmail.com>
2025-05-20 10:54:30 -0400

    Installed common middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// COMMON MIDDLEWARES
+
+// config CORS
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// config JSON data
+app.use(express.json({ limit: "16kb" }));
+
+// config URL-Encoding
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// config COOKIE-PARSER
+app.use(cookieParser());
+
+// config PUBLIC FOLDER
+app.use(express.static("public"));
 
 export { app };
