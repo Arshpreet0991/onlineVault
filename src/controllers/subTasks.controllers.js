57c2598a0857a4125645d11d1d8b3954ff99a70c
Arshpreet0991 <arsh.singh0991@gmail.com>
2025-05-28 10:56:33 -0400

    created the controller to add sub tasks

--- src/controllers/subTasks.controllers.js
+++ src/controllers/subTasks.controllers.js
@@ -6,9 +6,32 @@ import {
   usernameValidator,
   emailValidator,
   passwordValidator,
+  contentValidator,
 } from "../utils/validations.js";
 
 // create a sub task
-const createSubTask = asyncHandler(async (req, res) => {});
+const createSubTask = asyncHandler(async (req, res) => {
+  const { taskId, subTaskContent, dueAt, priority } = req.body;
+
+  const userId = req.user?._id;
+
+  const ifValid = contentValidator(subTaskContent);
+
+  if (!ifValid) {
+    throw new ApiError(400, "Content format not valid");
+  }
+
+  const subTask = await SubTask.create({
+    subTaskContent,
+    taskId,
+    createdBy: userId,
+    dueAt,
+    priority,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, subTask, "Sub Task Added Successfully"));
+});
 
 export { createSubTask };
